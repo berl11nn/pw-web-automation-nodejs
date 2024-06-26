9c4f2cb5b84a447baaec6133a3bc26de44f29d3c
Phat Le <phatl2511@gmail.com>
2024-06-26 21:21:16 +0700

    refactor 2.3

--- exercise/day-01/lab2/ExerciseThree.js
+++ exercise/day-01/lab2/ExerciseThree.js
@@ -9,19 +9,14 @@ let overWeight = 29.9;
 let suggestionWeight;
 
 if(BMI <= underWeight){
-    console.log(BMI);
     suggestionWeight = ((underWeight - BMI) * (Number(height) * 2))
-    console.log(Math.round(suggestionWeight));
     console.log('You should increase your weight more: ' + Math.round(suggestionWeight) + 'kg');
 }else if (BMI <= normalWeight){
     console.log('Keep maintaining your weight.');
 }else if(BMI <= overWeight){
     suggestionWeight = ((BMI - underWeight) * (Number(height) * 2))
-    console.log(Math.round(suggestionWeight));
     console.log('You should decrease your weight down: ' + Math.round(suggestionWeight) + 'kg');
 }else{
-    console.log(BMI);
     suggestionWeight = ((BMI - underWeight) * (Number(height) * 2))
-    console.log(Math.round(suggestionWeight));
     console.log('You should decrease your weight down: ' + Math.round(suggestionWeight) + 'kg');
 }
